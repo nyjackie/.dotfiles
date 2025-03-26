42814a57c5af1bd289c290ea028dc959e816abda
Jackie Xu <jackiexu1228@gmail.com>
2025-03-25 21:52:25 -0400

    update

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,16 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- local set = vim.keymap.set
+local set = vim.keymap.set
 opt = { noremap = true, silent = true }
--- Beginning of line
-set("i", "<C-h>", "<ESC>^i", opt)
 
 -- End of line
 set("i", "<C-l>", "<End>", opt)
-
 -- Insert end of line
 set("n", "<C-a>", "<End>a", opt)
 
--- Line break
-set("n", "<C-j>", "i<CR><ESC>", opt)
+-- Beggining of line
+set("i", "<C-h>", "<ESC>^i", opt)
